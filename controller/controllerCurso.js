444822b289dffbd32dde7261c61987d4540e9479
Cristina Credie <c2midia@gmail.com>
2021-06-10 22:00:19 -0300

    alteracao curso.ejs

--- controller/controllerCurso.js
+++ controller/controllerCurso.js
@@ -1,23 +1,18 @@
 const modelCurse = require("../model/modelCurso");
-
  async function get (req,res){
    const curse = await modelCurse.getCurso();
-   res.render("curso",{curse:curse});
+   res.render("curso",{courses:curse});
 }
-
 async function getEdit(req,res){
    const curseId = req.params.id;
    const curse = await modelCurse.getCurseById(curseId);
    res.render("curse/edit",{curse:curse});
 }
-
 async function post(req,res){
     const curse = req.body;
-    await modelCurse.insertCurse(curse);
-    res.redirect("/curse");
-
+    await modelCurse.insertCurso(curse);
+    res.redirect("/curso");
 }
-
 module.exports = {
    get:get,
    getEdit:getEdit,
